691b0376f24ebd290bf72d31d050bf7a6460eca3
Levente <122294490+Levi-24@users.noreply.github.com>
2024-11-19 10:16:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using berek2020.src;
+
+List<Dolgozo> dolgozok = new();
+StreamReader reader = new(@"../../../src/berek2020.txt");
+_= reader.ReadLine();
+while (!reader.EndOfStream)
+{
+    dolgozok.Add(new Dolgozo(reader.ReadLine()));
+}
+
+Console.WriteLine("3. Feladat:");
+Console.WriteLine("\t" + dolgozok.Count);
+
+Console.WriteLine("4. Feladat:");
+Console.WriteLine($"\t{ dolgozok.Average(d => d.Fizetes) / 1000:0.0}");
+
+Console.WriteLine("5. Feladat:");
+Console.Write("\tAdja meg egy részlegnek a nevét:");
+string keresettReszleg = Console.ReadLine();
+
+Console.WriteLine("6. Feladat:");
+var legnagyobbFizetes = dolgozok.Where(d => d.Reszleg == keresettReszleg).OrderByDescending(d => d.Fizetes).FirstOrDefault();
+Console.WriteLine("\t" + legnagyobbFizetes);
+
+Console.WriteLine("7. Feladat:");
+var reszlegStatisztika = dolgozok.GroupBy(d => d.Reszleg);
+foreach (var dolgozo in reszlegStatisztika)
+{
+    Console.WriteLine($"\t{dolgozo.Key}, {dolgozo.Count()}");
+}
+
+//Munkaidő: 00:25:10
---
+++ src/Dolgozo.cs
@@ -0,0 +1,26 @@
+﻿namespace berek2020.src
+{
+    internal class Dolgozo
+    {
+        public string Nev { get; set; }
+        public bool Nem { get; set; }
+        public string Reszleg { get; set; }
+        public int Belepes { get; set; }
+        public int Fizetes { get; set; }
+
+        public Dolgozo(string sor)
+        {
+            var adat = sor.Split(';');
+            Nev = adat[0];
+            Nem = adat[1] == "férfi";
+            Reszleg = adat[2];
+            Belepes = int.Parse(adat[3]);
+            Fizetes = int.Parse(adat[4]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev} ({(Nem ? "férfi" : "nő")}) {Reszleg} {Belepes} {Fizetes}Ft";
+        }
+    }
+}
